6c7ca30ff0e6a37c23b6f4d217dd6d6da590cc0a
Alma Santang <30685704+almasantang@users.noreply.github.com>
2021-04-21 15:46:29 +1000

    testing js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,26 @@
-$( document ).ready(function() {
-// start here  
+// $( document ).ready(function() {
+// // start here  
 
-$('.popup-link').click(function() {
-  $('.popup').addClass('active');
-});
+// $('.popup-link').click(function() {
+//   $('.popup').addClass('active');
+// });
 
-$('.popup').click(function() {
-  $('.popup').removeClass('active');
-});
+// $('.popup').click(function() {
+//   $('.popup').removeClass('active');
+// });
 
 
-function isMobile() {
-  $deviceList = array(
-    '/iphone/i' => 'iPhone', 
-    '/ipod/i' => 'iPod', 
-    '/ipad/i' => 'iPad', 
-    '/android/i' => 'Android'
-  );
+// function isMobile() {
+//   $deviceList = array(
+//     '/iphone/i' => 'iPhone', 
+//     '/ipod/i' => 'iPod', 
+//     '/ipad/i' => 'iPad', 
+//     '/android/i' => 'Android'
+//   );
 
-  foreach($deviceList as $deviceKey => $deviceOS) {
-    if(preg_match($deviceKey, $_SERVER['HTTP_USER_AGENT'])) {
-        // echo your content for mobile devices
-    }
-  }
-}
+//   foreach($deviceList as $deviceKey => $deviceOS) {
+//     if(preg_match($deviceKey, $_SERVER['HTTP_USER_AGENT'])) {
+//         // echo your content for mobile devices
+//     }
+//   }
+// }
