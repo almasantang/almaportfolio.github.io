110db3e16981913b83eb5d36b492d5fa63f258cb
Alma Santang <30685704+almasantang@users.noreply.github.com>
2021-04-21 15:43:58 +1000

    testing homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,19 @@ $('.popup-link').click(function() {
 $('.popup').click(function() {
   $('.popup').removeClass('active');
 });
+
+
+function isMobile() {
+  $deviceList = array(
+    '/iphone/i' => 'iPhone', 
+    '/ipod/i' => 'iPod', 
+    '/ipad/i' => 'iPad', 
+    '/android/i' => 'Android'
+  );
+
+  foreach($deviceList as $deviceKey => $deviceOS) {
+    if(preg_match($deviceKey, $_SERVER['HTTP_USER_AGENT'])) {
+        // echo your content for mobile devices
+    }
+  }
+}
