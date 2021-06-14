81f6fa5805cd7687a7b2927984d963ee72e118f7
Alma Santang <30685704+almasantang@users.noreply.github.com>
2021-06-15 01:22:11 +1000

    website update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,24 @@
-$( document ).ready(function() {
-// start here  
-
-$('.popup-link').click(function() {
-  $('.popup').addClass('active');
-});
-
-$('.popup').click(function() {
-  $('.popup').removeClass('active');
-});
-
-
-function isMobile() {
-  $deviceList = array(
-    '/iphone/i' => 'iPhone', 
-    '/ipod/i' => 'iPod', 
-    '/ipad/i' => 'iPad', 
-    '/android/i' => 'Android'
-  );
-
-  foreach($deviceList as $deviceKey => $deviceOS) {
-    if(preg_match($deviceKey, $_SERVER['HTTP_USER_AGENT'])) {
-        // echo your content for mobile devices
+// IMAGE SLIDESHOW
+    var slideIndex = [1, 1, 1, 1, 1];
+    var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"]
+    showSlides(1, 0);
+    showSlides(1, 1);
+    showSlides(1, 2);
+    showSlides(1, 3);
+    showSlides(1, 4);
+    
+    function plusSlides(n, no) {
+      showSlides(slideIndex[no] += n, no);
     }
-  }
-}
+    
+    function showSlides(n, no) {
+      var i;
+      var x = document.getElementsByClassName(slideId[no]);
+      if (n > x.length) {slideIndex[no] = 1}    
+      if (n < 1) {slideIndex[no] = x.length}
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";  
+      }
+      x[slideIndex[no]-1].style.display = "block";  
+    };
+// IMAGE SLIDESHOW
